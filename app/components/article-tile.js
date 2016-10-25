6e1460d4d69e2b72c200a445e89cd54e858c40c1
Katy Henning and Kevin Johnston <kevin@>
2016-10-25 14:43:44 -0700

    Add delete feature

--- app/components/article-tile.js
+++ app/components/article-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    delete(article) {
+      if (confirm('Are you sure?')) {
+        this.sendAction('destroyArticle', article);
+      }
+    }
+  }
 });
